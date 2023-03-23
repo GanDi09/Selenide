0c15c37ef0319aa71617a1ad8fb27a816ad14194
Rinal Shaihutdinov <iversonmelo.6@yandex.ru>
2023-03-23 21:39:19 +0300

    add test

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
@@ -13,14 +15,20 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
+   private String generateDate(int addDays, String pattern){
+       return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
     @Test
     void shouldDeliveryTest(){
         Configuration.holdBrowserOpen = true;
 
         open("http://localhost:9999");
         $("span[data-test-id=city] input").setValue("Казань");
-        //$("[data-test-id=date]").click();
-        //$("[data-test-id=date]").sendKeys(Keys.BACK_SPACE);
+        String currentDate = generateDate(5, "dd.MM.yyyy");
+        $("[data-test-id=date] input").doubleClick();
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").sendKeys(currentDate);
         $("span[data-test-id=name] input").setValue("Роман");
         $("span[data-test-id=phone] input").setValue("+76587654678");
         $("[data-test-id=agreement]").click();
